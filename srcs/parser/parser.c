a50c3b13c39194ea928416f99e8337b9a90c45c6
momeyer <meyer.monique93@gmail.com>
2021-07-13 16:25:50 +0200

    Added function to check last char of command

--- srcs/parser/parser.c
+++ srcs/parser/parser.c
@@ -1,5 +1,6 @@
 #include "parser.h"
 #include <string.h>
+#include <ctype.h>
 
 t_bool	parse_input(const char *input)
 {
@@ -13,9 +14,23 @@ t_bool	parse_input(const char *input)
 	return (TRUE);
 }
 
+static t_bool	is_valid_last_char(char last_char)
+{
+	return (isspace(last_char) || last_char == '\0');
+}
+
+static t_bool	is_command(const char *input, const char *command)
+{
+	const int		command_len = strlen(command);
+	const char		last_char = input[command_len];
+
+	return (strncmp(input, command, command_len) == 0
+		&& is_valid_last_char(last_char));
+}
+
 t_command	get_command(const char *input)
 {
-	if (strncmp(input, ECHO_STR, strlen(ECHO_STR)) == 0)
+	if (is_command(input, ECHO_STR))
 		return (ECHO);
 	return (INVALID);
 }
--- tests/parser_test.c
+++ tests/parser_test.c
@@ -6,7 +6,7 @@
 
 CTEST(parse_input, success_return)
 {
-    ASSERT_TRUE(parse_input(ECHO_STR));
+    ASSERT_TRUE(parse_input("echo "));
 }
 
 CTEST(parse_input, no_line_to_parse)
@@ -17,4 +17,19 @@ CTEST(parse_input, no_line_to_parse)
 CTEST(get_command, invalid_command)
 {
     ASSERT_EQUAL(INVALID, get_command("ecth"));
+}
+
+CTEST(get_command, invalid_echo_command)
+{
+    ASSERT_EQUAL(INVALID, get_command("echos"));
+}
+
+CTEST(get_command, echo_command)
+{
+    ASSERT_EQUAL(ECHO, get_command("echo"));
+}
+
+CTEST(get_command, echo_command_with_arg)
+{
+    ASSERT_EQUAL(ECHO, get_command("echo hello"));
 }
