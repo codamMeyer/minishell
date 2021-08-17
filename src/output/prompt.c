daea77bb02a2e3d901036f3fdc381cb3cb5fafca
momeyer <meyer.monique93@gmail.com>
2021-08-17 14:40:20 +0200

    Added freature to get pwf and display on prompt

--- src/output/prompt.c
+++ src/output/prompt.c
@@ -1,7 +1,21 @@
 #include <stdio.h>
 #include <output/prompt.h>
+#include <unistd.h>
+#include <libft.h>
+#define COLOR "\e[1;36m "
+#define RESET_COLOR " -►\e[0m  "
 
-void	dispaly_prompt(void)
+char	*display_prompt(char *buffer)
 {
-	printf("BestShellEver -> ");
+	const char	*path = getcwd(NULL, 0);
+	const int	path_len = ft_strlen(path);
+	const int	color_len = ft_strlen(COLOR);
+	const int	reset_color_len = ft_strlen(RESET_COLOR);
+
+	ft_bzero(buffer, sizeof(buffer));
+	ft_strlcpy(buffer, COLOR, color_len);
+	ft_strlcpy(&buffer[ft_strlen(buffer)], path, path_len);
+	ft_strlcpy(&buffer[ft_strlen(buffer)], RESET_COLOR, reset_color_len);
+	free((char *)path);
+	return (buffer);
 }
