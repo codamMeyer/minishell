0f91a0d628e2fd2dae0ebfd03706a08d0d745ca1
Ryno Meiboom <rmeiboom@f0r6s2.codam.nl>
2021-08-31 18:40:17 +0200

    started parsing angled brackets

--- src/commands/commands.h
+++ src/commands/commands.h
@@ -3,6 +3,7 @@
 # define WHITESSPACE_AND_QUOTES " 	\""
 # define WHITESSPACE " \t"
 # define N_FLAG "-n"
+# define REDIRECTION_CHARS "<>" 
 
 # include <defines.h>
 
@@ -11,6 +12,8 @@ typedef enum e_special_chars
 	SPACE = ' ',
 	NULL_TERMINATOR = '\0',
 	PIPE = '|',
+	LEFT_ANGLE = '<',
+	RIGHT_ANGLE = '>'
 }	t_special_chars;
 
 typedef struct s_arg
@@ -19,12 +22,19 @@ typedef struct s_arg
 	const char	*end;
 }	t_arg;
 
+typedef struct s_files
+{
+	const char		*out;
+	const char		*in;
+}	t_files;
+
 typedef struct s_command
 {
 	t_command_code	code;
 	t_arg			arg;
 	int				arg_len;
 	const char		*exe_path;
+	t_files			files;
 }	t_command;
 
 typedef void	(*t_output_stdout)(const char *);
--- src/parser/parser.c
+++ src/parser/parser.c
@@ -18,11 +18,51 @@ static void	consume_pipe(const char **input, int index)
 		++(*input);
 }
 
+void init_files(t_files *files)
+{
+	files->in = NULL;
+	files->out = NULL;
+}
+
+t_files	get_in_out_file(const char **input_ptr, const char id)
+{
+	int		file_name_length;
+	t_files	files;
+
+	init_files(&files);
+	++(*input_ptr);
+	skip_spaces(input_ptr);
+	file_name_length = get_cmd_len(*input_ptr);
+	if (id == LEFT_ANGLE)
+		files.in = *input_ptr;
+	else if (id == RIGHT_ANGLE)
+		files.out = *input_ptr;
+	*input_ptr += file_name_length;
+	return (files);
+}
+
+static t_files get_redirection(const char **input_ptr, int index)
+{
+	t_files	files;
+
+	skip_spaces(input_ptr);
+	init_files(&files);
+	if (**input_ptr == PIPE)
+	{
+		consume_pipe(input_ptr, index);
+		return (files);
+	}
+	if (**input_ptr == LEFT_ANGLE || **input_ptr == RIGHT_ANGLE)
+		files = get_in_out_file(input_ptr, **input_ptr);
+	return (files);
+}
+
 /* display syntax error when necessary */
-t_command	populate_command(const char **input_ptr)
+t_command	populate_command(const char **input_ptr, int index)
 {
 	t_command	command;
 
+	command.files = get_redirection(input_ptr, index);
 	command.code = get_command_code(input_ptr, &command);
 	command.arg.start = *input_ptr;
 	if (command.code == INVALID)
@@ -32,7 +72,10 @@ t_command	populate_command(const char **input_ptr)
 	return (command);
 }
 
-/* need to decide how to handle pipes */
+/* 
+	need to decide how to handle pipes
+	if invalid command, should still continue parsing
+*/
 int	populate_commands_table(const char *input, t_command commands_table[])
 {
 	const char	*input_line = input;
@@ -43,8 +86,7 @@ int	populate_commands_table(const char *input, t_command commands_table[])
 	i = 0;
 	while (*input_line)
 	{
-		consume_pipe(&input_line, i);
-		commands_table[i] = populate_command(&input_line);
+		commands_table[i] = populate_command(&input_line, i);
 		if (commands_table[i].code == INVALID)
 		{
 			++i;
