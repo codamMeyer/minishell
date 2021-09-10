ee095d4f31d95df9b48a260a634b7ac63fd9b4ad
momeyer <meyer.monique93@gmail.com>
2021-09-10 17:00:17 +0200

    Added some tests for pipes and env, but need to refactor

--- tests/acceptance/env_feature_bash.sh
+++ tests/acceptance/env_feature_bash.sh
@@ -12,9 +12,10 @@ EXPECTED=$(env | grep TEST_VAR)
 assertEqual "$1"
 
 TEST="export TEST_1=Testing"
-echo -e "$TEST\nenv | grep TEST_1 exit" | ./minishell > $MININHELL_OUTPUT
-ACTUAL=$(sed 4p $MININHELL_OUTPUT)
-EXPECTED=$(env | grep TEST_VAR)
+echo -e "$TEST\nenv | grep TEST_1\nexit\n" | ./minishell > $MININHELL_OUTPUT
+ACTUAL=$(sed -n 3p $MININHELL_OUTPUT)
+export TEST_1=Testing
+EXPECTED=$(env | grep TEST_1)
 assertEqual "$1"
 
 cleanUp
