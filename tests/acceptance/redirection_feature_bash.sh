5896581194f54fcc9074688601dc832335e9e0f3
Rynoster <Ryno.Meiboom@gmail.com>
2021-09-15 15:58:23 +0200

    added basic test example for redirections

--- tests/acceptance/redirection_feature_bash.sh
+++ tests/acceptance/redirection_feature_bash.sh
@@ -1,19 +1,23 @@
 source ./tests/acceptance/common.sh
 
+# function to loop through file (num file)
+	# function check_that_file_exists
+	# check content
+
 function check_file_content ()
 {
-	if [ -f "$MINI_REDIRECT_OUT" ]; then
-		removePrompt "$MINI_REDIRECT_OUT"
-		ACTUAL=$(cat "$MINI_REDIRECT_OUT")
+	if [ -f "$1" ]; then
+		removePrompt "$1"
+		ACTUAL=$(cat "$1")
 	fi
-	EXPECTED=$(cat "$BASH_REDIRECT_OUT")
+	EXPECTED=$(cat "$2")
 }
 
 printTestName "Redirection"
 
-INPUT="echo"
+INPUT="echo first test > $MINI_REDIRECT_OUT"
 echo first test > "$BASH_REDIRECT_OUT"
 runMinishell "$INPUT"
-check_file_content
+check_file_content "$MINI_REDIRECT_OUT" "$BASH_REDIRECT_OUT"
 assertEqual "Redirection basic"
 cleanUp
