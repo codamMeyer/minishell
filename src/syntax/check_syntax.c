9a2118ed031535dd3c98bf7c21140a15999fa358
Ryno Meiboom <rmeiboom@f0r3s6.codam.nl>
2021-09-25 14:23:17 +0200

    covered pipes: lastcahr, firstchar, doublepipes, valid pipe

--- src/syntax/check_syntax.c
+++ src/syntax/check_syntax.c
@@ -1,5 +1,11 @@
 #include <defines.h>
 #include <commands/commands.h>
+#include <parser/command_table.h>
+#include <parser/parser.h>
+#include <libft.h>
+#include <ctype.h>
+#include <stdio.h>
+
 
 t_bool	is_valid_angled_brackets_syntax(const char *input)
 {
@@ -7,10 +13,47 @@ t_bool	is_valid_angled_brackets_syntax(const char *input)
 	return (TRUE);
 }
 
+t_bool	only_contains_white_space_after_pipe(const char *str)
+{
+	while (str && *str)
+	{
+		if (!isspace(*str))
+			return (FALSE);
+		++str;
+	}
+	return (TRUE);
+}
+
 t_bool	is_valid_pipes_syntax(const char *input)
 {
-	if (*input == PIPE)
-		return (FALSE);
+	const int	len  = ft_strlen(input);
+	int			i;
+
+	i = 0;
+	while (input && input[i] && i < len)
+	{
+		skip_spaces(&input);
+		i += get_arg_len(&input[i], "|");
+		printf("\ni = %d\nlen = %d\n", i, len);
+		if (i == len && input[i] != PIPE)
+			break ;
+		if (input[i + 1] == PIPE)
+		{
+			printf("\nDOUBLE PIPES\n");
+			return (FALSE);
+		}
+		else if (only_contains_white_space_after_pipe(&input[i + 1]))
+		{
+			printf("\nLAST CHAR PIPE\n");
+			return (FALSE);
+		}
+		if (i == 0)
+		{
+			printf("\nFIRST CHAR PIPE\n");
+			return (FALSE);
+		}
+		++i;
+	}
 	return (TRUE);
 }
 
--- tests/syntax_checker_tests.c
+++ tests/syntax_checker_tests.c
@@ -6,7 +6,22 @@ CTEST(PIPES, VALID_PIPE)
     ASSERT_EQUAL(TRUE, is_valid_syntax("echo hallo | cat -e"));
 }
 
-CTEST(PIPES, INVALID_PIPE)
+CTEST(PIPES, INVALID_PIPE_FIRST_CHAR)
 {
     ASSERT_EQUAL(FALSE, is_valid_syntax("| echo hallo"));
+}
+
+CTEST(PIPES, INVALID_PIPE_LAST_CHAR)
+{
+    ASSERT_EQUAL(FALSE, is_valid_syntax("echo hallo|"));
+}
+
+CTEST(PIPES, INVALID_PIPE_LAST_CHAR_BEFORE_WHITESPACE)
+{
+    ASSERT_EQUAL(FALSE, is_valid_syntax("echo hallo      |           "));
+}
+
+CTEST(PIPES, DOUBLE_PIPES)
+{
+    ASSERT_EQUAL(FALSE, is_valid_syntax("echo hallo      || cat -e           "));
 }
