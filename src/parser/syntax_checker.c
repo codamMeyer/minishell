9190c4eb5123372b6eb3dd22e68767ded6888b89
momeyer <meyer.monique93@gmail.com>
2021-09-27 18:22:11 +0200

    Fixed norm on new files

--- src/parser/syntax_checker.c
+++ src/parser/syntax_checker.c
@@ -1,26 +1,26 @@
 #include <parser/syntax_checker.h>
 #include <commands/quotes.h>
 
-t_exit_code is_missing_quotes(const char *input, t_output_stdout output) 
+t_exit_code	is_missing_quotes(const char *input, t_output_stdout output)
 {
-    t_quotes_position	quotes;
+	t_quotes_position	quotes;
 
-    while (*input && !is_quote(*input))
-        ++input;
-    if (!*input)
-        return (SUCCESS);
-    quotes = get_quotes_positions(input);
-    while (quotes.start && quotes.end)
-    {
-        ++quotes.end;
-        while (*quotes.end && !is_quote(*quotes.end))
-            ++quotes.end;
-        quotes = get_quotes_positions(quotes.end);
-    }
-    if (quotes.start)
-    {
-        output("Missing closing quote\n");
-        return (ERROR);
-    }
-    return (SUCCESS);
+	while (*input && !is_quote(*input))
+		++input;
+	if (!*input)
+		return (SUCCESS);
+	quotes = get_quotes_positions(input);
+	while (quotes.start && quotes.end)
+	{
+		++quotes.end;
+		while (*quotes.end && !is_quote(*quotes.end))
+			++quotes.end;
+		quotes = get_quotes_positions(quotes.end);
+	}
+	if (quotes.start)
+	{
+		output("Missing closing quote\n");
+		return (ERROR);
+	}
+	return (SUCCESS);
 }
