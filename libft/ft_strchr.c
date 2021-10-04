949ab52b700793af3170fbbe586e869e53b86e58
Ryno Meiboom <rmeiboom@f1r2s18.codam.nl>
2021-10-04 16:44:20 +0200

    removed garbage from libft

--- libft/ft_strchr.c
+++ libft/ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: rmeiboom <rmeiboom@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/11/14 14:07:44 by rmeiboom      #+#    #+#                 */
-/*   Updated: 2021/09/27 22:43:48 by rmeiboom      ########   odam.nl         */
+/*   Updated: 2021/10/04 16:43:52 by rmeiboom      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,6 @@ char	*ft_strchr(const char *s, int c)
 	i = 0;
 	if (c == '\0' && str[len] == c)
 	{
-		printf("return here\n");
 		cursor = &str[len];
 		return (cursor);
 	}
