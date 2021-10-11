9ce464c4ec25ad9489bfd66adbe6d78db9ab527f
momeyer <meyer.monique93@gmail.com>
2021-10-11 17:49:43 +0200

    Fixed norm

--- main.c
+++ main.c
@@ -11,11 +11,11 @@
 
 static void	add_key_and_value_from_env(char *env_str_pair)
 {
-	char	*value;
+	char		*value;
 	t_buffer	key;
 
 	init_buffer(&key);
-	copy_key_to_buffer(env_str_pair, &key);	
+	copy_key_to_buffer(env_str_pair, &key);
 	set_key(get_environment(), key.buf);
 	value = get_equal_sign_position(env_str_pair) + 1;
 	set_value(get_environment(), key.buf, value);
