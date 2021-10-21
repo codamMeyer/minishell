0233b26d7cd181f2a531e69b9542eaa1ac1397d0
Ryno Meiboom <rmeiboom@f1r1s11.codam.nl>
2021-10-21 12:25:09 +0200

    we moved the restore_std_fds to cover both cases

--- src/executor/run_commands.c
+++ src/executor/run_commands.c
@@ -64,12 +64,12 @@ int	run_commands(t_command commands[],
 	{
 		redirect_in_and_output(NULL, 0, 0, &commands[0]);
 		dispatch_command(&commands[0], env);
-		restore_std_fds(fds);
 	}
 	else
 	{
 		run_multi_processes(env, commands, num_of_commands);
 		wait_for_all_processes(num_of_commands);
 	}	
+	restore_std_fds(fds); 
 	return (SUCCESS);
 }
