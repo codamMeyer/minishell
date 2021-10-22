e4b551232b168913d414091357f92cd3bd798d68
momeyer <meyer.monique93@gmail.com>
2021-10-22 12:14:42 +0200

    Fixed variable expansion to trim when nedded

--- src/commands/buffer.c
+++ src/commands/buffer.c
@@ -3,6 +3,7 @@
 #include <commands/quotes.h>
 #include <env/environment.h>
 #include <env/env_utils.h>
+#include <parser/parser.h>
 
 void	init_buffer(t_buffer *buffer)
 {
@@ -17,23 +18,35 @@ void	append_char_to_buffer(t_arg *arg, t_buffer *buffer)
 	++(arg->start);
 }
 
-void	append_env_value_to_buffer(t_arg *arg, t_buffer *buffer)
+void	append_env_value_to_buffer(t_arg *arg, t_buffer *buffer, t_bool trimmed)
 {
-	t_env			*var;
-	int				key_len;
-	int				value_len;
+	t_env	*var;
+	int		key_len;
+	char	*value;
 
 	++(arg->start);
-	value_len = 0;
 	key_len = get_key_len(arg->start);
 	var = find_variable(get_environment(), arg->start);
 	if (var)
 	{
-		value_len = ft_strlen(var->value);
-		ft_strlcpy(&buffer->buf[buffer->index], var->value, value_len + 1);
+		value = var->value;
+		if (trimmed)
+			skip_spaces((const char **)&value);
+		while (*value)
+		{
+			if (buffer->buf[buffer->index] == SPACE_CHAR && trimmed)
+				skip_spaces((const char **)&value);
+			buffer->buf[buffer->index] = *value;
+			++value;
+			++(buffer->index);
+		}
+		if (trimmed && buffer->buf[buffer->index - 1] == SPACE_CHAR)
+		{
+			--(buffer->index);
+			buffer->buf[buffer->index] = NULL_TERMINATOR;
+		}
 	}
 	arg->start = arg->start + key_len;
-	buffer->index += value_len;
 }
 
 void	append_quoted_string_to_buffer(t_arg *arg, t_buffer *buffer)
@@ -46,7 +59,7 @@ void	append_quoted_string_to_buffer(t_arg *arg, t_buffer *buffer)
 		while (arg->start < quotes.end)
 		{
 			if (quotes.is_double_quote && is_env_variable(arg->start))
-				append_env_value_to_buffer(arg, buffer);
+				append_env_value_to_buffer(arg, buffer, FALSE);
 			else
 				append_char_to_buffer(arg, buffer);
 		}
@@ -61,7 +74,7 @@ void	append_expanded_input_to_buffer(t_arg *arg, t_buffer *buffer)
 	if (is_quote(*arg->start))
 		append_quoted_string_to_buffer(arg, buffer);
 	else if (is_env_variable(arg->start))
-		append_env_value_to_buffer(arg, buffer);
+		append_env_value_to_buffer(arg, buffer, TRUE);
 	else
 		append_char_to_buffer(arg, buffer);
 }
