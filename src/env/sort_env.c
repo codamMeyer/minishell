7bcf47b2f3ec2fdeb995e9f843fd328d66d68f7e
momeyer <meyer.monique93@gmail.com>
2021-10-26 11:57:21 +0200

    Changed buffer  name to index_buffer

--- src/env/sort_env.c
+++ src/env/sort_env.c
@@ -4,7 +4,7 @@
 #include <libft.h>
 #include <stdio.h>
 
-int	populate_env_index_buffer(int buffer[])
+int	populate_env_index_buffer(int index_buffer[])
 {
 	t_env	*env;
 	int		i;
@@ -17,7 +17,7 @@ int	populate_env_index_buffer(int buffer[])
 	{
 		if (env[i].key)
 		{
-			buffer[j] = i;
+			index_buffer[j] = i;
 			++j;
 		}
 		++i;
@@ -25,9 +25,9 @@ int	populate_env_index_buffer(int buffer[])
 	return (j);
 }
 
-int	sort_env(const t_env *env, int buffer[])
+int	sort_env(const t_env *env, int index_buffer[])
 {
-	const int	num_vars = populate_env_index_buffer(buffer);
+	const int	num_vars = populate_env_index_buffer(index_buffer);
 	t_bool		swapped;
 	int			tmp;
 	int			i;
@@ -39,11 +39,11 @@ int	sort_env(const t_env *env, int buffer[])
 		swapped = FALSE;
 		while (i < num_vars - 1)
 		{
-			if (ft_strcmp(env[buffer[i]].key, env[buffer[i + 1]].key) > 0)
+			if (ft_strcmp(env[index_buffer[i]].key, env[index_buffer[i + 1]].key) > 0)
 			{
-				tmp = buffer[i];
-				buffer[i] = buffer[i + 1];
-				buffer[i + 1] = tmp;
+				tmp = index_buffer[i];
+				index_buffer[i] = index_buffer[i + 1];
+				index_buffer[i + 1] = tmp;
 				swapped = TRUE;
 			}
 			++i;
@@ -55,18 +55,18 @@ int	sort_env(const t_env *env, int buffer[])
 void	display_sorted_env(void)
 {
 	const t_env	*env = get_environment();
-	int			buffer[ENV_SIZE];
+	int			index_buffer[ENV_SIZE];
 	int			num_elements;
 	int			i;
 
-	num_elements = sort_env(env, buffer);
+	num_elements = sort_env(env, index_buffer);
 	i = 0;
 	while (i < num_elements)
 	{
-		if (ft_strncmp(env[buffer[i]].key, "_", 2) != 0)
+		if (ft_strncmp(env[index_buffer[i]].key, "_", 2) != 0)
 		{
 			printf("declare -x ");
-			printf("%s=\"%s\"\n", env[buffer[i]].key, env[buffer[i]].value);
+			printf("%s=\"%s\"\n", env[index_buffer[i]].key, env[index_buffer[i]].value);
 		}
 		++i;
 	}
