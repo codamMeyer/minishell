ed4ada22b1ca39bc377c7f6992e6c4fc880cb894
Ryno Meiboom <rmeiboom@f1r1s17.codam.nl>
2021-10-27 12:14:03 +0200

    fixed os type in heredoc test

--- tests/acceptance/heredoc_feature_bash.sh
+++ tests/acceptance/heredoc_feature_bash.sh
@@ -4,13 +4,23 @@ source ./tests/acceptance/common.sh
 
 printTestName "HEREDOC"
 
+function removeHeredocPrompt ()
+{
+    if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+        sed -i /">"/d $MINISHELL_OUTPUT
+    elif [[ "$OSTYPE" == "darwin"* ]]; then
+        sed -i "" /">"/d $MINISHELL_OUTPUT
+    fi
+}
+
+
 echo -e "cat -e << EOF
 Hello world
 Today date is $(date +%F)
 My home directory = ${HOME}
 EOF" | ./minishell > $MINISHELL_OUTPUT
 removePrompt $MINISHELL_OUTPUT
-sed -i "" /">"/d $MINISHELL_OUTPUT
+removeHeredocPrompt
 ACTUAL=$(cat $MINISHELL_OUTPUT)
 
 EXPECTED="$(cat -e << EOF
