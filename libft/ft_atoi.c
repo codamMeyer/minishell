0353c0ffd427a26cdad8d7894bb09a1eef5ea2bd
Ryno Meiboom <rmeiboom@f1r1s4.codam.nl>
2021-11-04 11:34:32 +0100

    added basic version

--- libft/ft_atoi.c
+++ libft/ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: rmeiboom <rmeiboom@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/11/14 14:06:09 by rmeiboom      #+#    #+#                 */
-/*   Updated: 2021/05/15 12:16:07 by rmeiboom      ########   odam.nl         */
+/*   Updated: 2021/11/04 11:31:41 by rmeiboom      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,7 +33,7 @@ int	ft_atoi(const char *str)
 	{
 		digit = (digit * 10) + (str[i] - '0');
 		if ((digit * negative) < LONG_MIN)
-			return (0);
+			return (-1);
 		else if ((digit * negative) > LONG_MAX)
 			return (-1);
 		i++;
--- src/commands/exit_command.c
+++ src/commands/exit_command.c
@@ -1,7 +1,22 @@
 #include <libft.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include <commands/commands.h>
 #include <parser/parser.h>
+
+static	t_bool has_only_nums(const char *str, int size)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] && i < size)
+	{
+		if (!ft_isdigit(str[i]) && str[i] != '-')
+			return (FALSE);
+		i++;
+	}
+	return (TRUE);
+}
 
 t_exit_code	exit_command(t_command command, t_output_stdout write_to_stdout)
 {
@@ -10,6 +25,10 @@ t_exit_code	exit_command(t_command command, t_output_stdout write_to_stdout)
 	(void)write_to_stdout;
 	skip_spaces(&command.arg.start);
 	exit_code = ft_atoi(command.arg.start);
+	if (!has_only_nums(command.arg.start, command.arg.len)) // exit code should be 255 on error
+		printf("BestShellEver: exit: %.*s: numeric argument required\n", command.arg.len, command.arg.start);
+	else if (exit_code == -1)
+		exit_code = 255;
 	exit(exit_code);
 	return (exit_code);
 }
