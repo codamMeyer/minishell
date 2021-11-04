3720f66d77ea6db94542bb3f2a921ab315a5867a
momeyer <meyer.monique93@gmail.com>
2021-11-04 20:29:40 +0100

    Added color to arrow

--- src/output/prompt.c
+++ src/output/prompt.c
@@ -5,14 +5,18 @@
 #include <env/environment.h>
 #include <output/prompt.h>
 
-#define COLOR "\e[1;36m"
+#define BLUE "\e[1;36m"
 #define RED "\e[1;31m"
-#define RESET_COLOR " -► \e[0m "
+#define GREEN "\e[1;32m"
+
+#define RED_ARROW RED" -► \e[0m "
+#define GREEN_ARROW GREEN" -► \e[0m "
 
 char	*display_prompt(char *buffer)
 {
-	const int	color_len = ft_strlen(COLOR) + 1;
-	const int	reset_color_len = ft_strlen(RESET_COLOR);
+	const int	color_len = ft_strlen(BLUE) + 1;
+	const int	reset_ok_len = ft_strlen(RED_ARROW);
+	const int	reset_error_len = ft_strlen(GREEN_ARROW);
 	t_buffer	path;
 	int			path_len;
 
@@ -22,12 +26,15 @@ char	*display_prompt(char *buffer)
 	{
 		ft_strlcpy(buffer, RED, color_len);
 		ft_strlcpy(&buffer[ft_strlen(buffer)], "WHY THOUGH???", 14);
-		ft_strlcpy(&buffer[ft_strlen(buffer)], RESET_COLOR, reset_color_len);
+		ft_strlcpy(&buffer[ft_strlen(buffer)], RED_ARROW, reset_error_len);
 		return (buffer);
 	}
 	path_len = ft_strlen(path.buf) + 1;
-	ft_strlcpy(buffer, COLOR, color_len);
+	ft_strlcpy(buffer, BLUE, color_len);
 	ft_strlcpy(&buffer[ft_strlen(buffer)], path.buf, path_len);
-	ft_strlcpy(&buffer[ft_strlen(buffer)], RESET_COLOR, reset_color_len);
+	if (*(get_exit_code()) != SUCCESS)
+		ft_strlcpy(&buffer[ft_strlen(buffer)], RED_ARROW, reset_error_len);
+	else
+		ft_strlcpy(&buffer[ft_strlen(buffer)], GREEN_ARROW, reset_ok_len);
 	return (buffer);
 }
