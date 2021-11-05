1490d42cd01d6d65666a0da33987ee7a14d7eb2a
Ryno Meiboom <rmeiboom@f0r1s4.codam.nl>
2021-11-05 15:36:34 +0100

    added a check for notspace and notnullchar

--- tests/acceptance/edge_cases_feature.sh
+++ tests/acceptance/edge_cases_feature.sh
@@ -33,4 +33,15 @@ assertEqual "Passing updated env"
 unset LS
 cleanUp
 
+STD=$(echo -e "cat /dev/urandom | base64 | head -c 100 | wc -c\nexit" | ./minishell > 1)
+removePrompt 1
+ACTUAL=$(cat 1)
+export LS="s -l"
+EXPECTED=$(cat /dev/urandom | base64 | head -c 100 | wc -c)
+assertEqual "Weird random test"
+unset LS
+rm 1
+cleanUp
+
+
 exit $EXIT_CODE
