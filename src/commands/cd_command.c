d4991d500272fba315c9e3a6b77da920cec202d7
momeyer <meyer.monique93@gmail.com>
2021-11-09 15:22:42 +0100

    Fixed CD

--- src/commands/cd_command.c
+++ src/commands/cd_command.c
@@ -11,8 +11,13 @@
 static void	update_env_value(const char *key, const char *new_value)
 {
 	t_env	*var;
+	char	*args[3];
 
-	// export(get_environment(), key);
+	args[0] = NULL;
+	args[1] = (char *)key;
+	args[2] = NULL;
+
+	export(get_environment(), args);
 	var = find_variable(get_environment(), key);
 	free(var->value);
 	var->value = ft_strdup(new_value);
@@ -54,15 +59,11 @@ t_exit_code	cd_command(t_command command, t_output_stdout output)
 	ret = SUCCESS;
 	getcwd(cwd_before_cd, BUFFER_SIZE);
 	init_buffer(&buffer);
-	if (command.arg.len == 0 || *command.arg.start == '~')
+	if (!command.arguments[1] || !ft_strcmp(command.arguments[1], "~"))
 		ret = copy_home_var_to_buffer(buffer.buf);
 	else
-	{
-		while (command.arg.start < command.arg.end && \
-				!ft_isspace(*command.arg.start))
-			append_expanded_input_to_buffer(&command.arg, &buffer);
-	}
-	if (chdir(buffer.buf) == SYS_ERROR && !ret)
+		ft_strlcpy(buffer.buf, command.arguments[1], ft_strlen(command.arguments[1]) + 1);
+	if (chdir(buffer.buf) == SYS_ERROR && ret == SUCCESS)
 	{
 		handle_error(SYS_ERROR, "cd", buffer.buf);
 		ret = ERROR;
